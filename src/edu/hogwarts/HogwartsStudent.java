74a008e67d3d1d633be3e3e6f4085115e1e79451
emilvn <emilvnielsen@hotmail.com>
2024-02-01 14:41:04 +0100

    tostring in hogwartsstudent

--- src/edu/hogwarts/HogwartsStudent.java
+++ src/edu/hogwarts/HogwartsStudent.java
@@ -4,4 +4,14 @@ public class HogwartsStudent extends Student implements HogwartsPerson {
     private House house;
     private boolean prefect;
     private String[] teams;
+
+    @Override
+    public String toString() {
+        StringBuilder teamString = new StringBuilder();
+        for(String team : teams){
+            teamString.append(team).append(", ");
+        }
+        teamString.replace(teamString.lastIndexOf(", "), teamString.length(), ".");
+        return super.toString() + "\nHouse: " + house + "\nPrefect: " + prefect + "\nTeams: " + teamString;
+    }
 }
